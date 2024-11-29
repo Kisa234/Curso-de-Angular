56ed5323fed6d7314728ece81142d5137f3cb7f9
Kisa234 <110016263+Kisa234@users.noreply.github.com>
2024-11-29 08:58:16 -0500

    update progress

---
+++ 06-heroesApp/src/app/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthRoutingModule } from './auth-routing.module';
+
+
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { LogingPageComponent } from './pages/loging-page/loging-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    LayoutPageComponent,
+    LogingPageComponent,
+    RegisterPageComponent
+
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+  ]
+})
+export class AuthModule { }
