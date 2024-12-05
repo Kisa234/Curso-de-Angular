4313198953203c7c64889220374c7a7f611fe035
Kisa234 <110016263+Kisa234@users.noreply.github.com>
2024-12-05 08:51:59 -0500

    update progress

--- 06-heroesApp/src/app/auth/auth-routing.module.ts
+++ 06-heroesApp/src/app/auth/auth-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { LogingPageComponent } from './pages/loging-page/loging-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
-const routes: Routes=[]
+
+//LOCALHOST:4200/auth/
+const routes: Routes=[
+  {
+      path:'',
+      component:LayoutPageComponent,
+      children:[
+        {path:'login', component: LogingPageComponent},
+        {path:'new-account', component: RegisterPageComponent},
+        {path:'**', redirectTo:'login'},
+      ]
+
+  },
+]
 
 @NgModule({
   imports: [ RouterModule.forChild(routes)],
